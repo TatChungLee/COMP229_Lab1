9d522ea88d5e6d5cbd7e1470550d8a492341f13a
Tat Chung Lee <chung5462@gmail.com>
2023-05-16 19:10:33 -0400

    first commit

---
+++ ArrowFunction/main.js
@@ -0,0 +1,11 @@
+const sum1 = (a, b) => {
+    return a + b;
+}
+let result = sum1(1, 2);
+console.log(result);
+
+// Or
+
+const sum2 = (a, b) => a + b;
+let result2 = sum2(1, 2);
+console.log(result2);
